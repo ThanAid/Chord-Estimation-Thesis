3da0390052fb5bd225f5b7d915ef0dc4c49f548d
thanos aidinis <thanos.aidinis@gmail.com>
2023-10-13 13:00:09 +0300

    added converter

---
+++ convert_mp3_wav.py
@@ -0,0 +1,58 @@
+# import required modules
+import subprocess
+import argparse
+from pydub import AudioSegment
+import os
+from loguru import logger
+
+
+def parse_input(args=None):
+    """
+       Parse cmd line arguments
+       :param args: The command line arguments provided by the user
+       :return: The parsed input Namespace
+       """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-dir", "--directory", type=str, action="store", metavar="directory",
+                        required=True)
+    parser.add_argument("-mn", "--mono", type=bool, action="store", metavar="mono",
+                        required=False)
+
+    return parser.parse_args(args)
+
+
+class ConvertAudio:
+    def __init__(self, direc, mono):
+        self.direc = direc
+        self.mono = mono
+        self.files = None
+
+        self.get_file_list()
+        self.convert()
+
+    def mp3_to_wav(self, file):
+        subprocess.call(['ffmpeg', '-i', file,
+                         file.split('.mp3')[0] + '.wav'])
+
+    def stereo_to_mono(self, file):
+        sound = AudioSegment.from_wav(file.split('.mp3')[0] + '.wav')
+        sound = sound.set_channels(1)
+        sound.export(file.split('.mp3')[0] + ".wav", format="wav")
+
+    def get_file_list(self):
+        self.files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(self.direc) for f in filenames if
+                      os.path.splitext(f)[1] == '.mp3']
+
+    def convert(self):
+        for file in self.files:
+            logger.info(f'Converting {os.path.basename(file)} to wav...')
+            self.mp3_to_wav(file)
+            if self.mono:
+                logger.info(f'Converting {os.path.basename(file)} to mono...')
+                self.stereo_to_mono(file)
+
+
+if __name__ == "__main__":
+    ARGS = parse_input()
+    ConvertAudio(ARGS.directory, ARGS.mono)
