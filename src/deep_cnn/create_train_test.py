e3fd3f58bfe348bfee7d4ae9b6494d993df9b540
ThanAid <thanos.aidinis@gmail.com>
2024-01-19 15:33:05 +0200

    Deeper CNN approach using data cache

---
+++ src/deep_cnn/create_train_test.py
@@ -0,0 +1,74 @@
+import pickle
+import sys
+
+from sklearn.preprocessing import LabelEncoder
+
+sys.path.append("../src")
+
+from src.utils.create_dataset import *
+from src.adapt_labels import *
+
+
+def main(dataset_paths):
+    """
+
+    :param dataset_paths:
+    :return:
+    """
+
+    # Split dataset by tracks
+    logger.info("Splitting dataset...")
+    df_train, df_test = split_dataset(dataset_paths, test_size=0.2, random_state=42)
+    logger.info("Reading and concatenating train dataset...")
+    df_train = read_and_concatenate_files(df_train, dataframe=True)
+    logger.info("Reading and concatenating test dataset...")
+    df_test = read_and_concatenate_files(df_test, dataframe=True)
+
+    # Extract features and labels
+    X_train = df_train.drop('labels', axis=1).values
+    X_test = df_test.drop('labels', axis=1).values
+    y_train_features = ConvertLab(df_train, label_col='labels', dest=None, is_df=True)
+    y_train = y_train_features.df['root'].values
+
+    y_test_features = ConvertLab(df_test, label_col='labels', dest=None, is_df=True)
+    y_test = y_test_features.df['root'].values
+
+    # Initialize LabelEncoder
+    label_encoder = LabelEncoder()
+
+    # Fit and transform all the labels
+    y_encoded = label_encoder.fit_transform(np.concatenate((y_test, y_train)))
+
+    # Transform the train and test labels using the same encoder
+    y_train_encoded = label_encoder.transform(y_train)
+    y_test_encoded = label_encoder.transform(y_test)
+
+    # Now y_train_encoded and y_test_encoded contain the encoded labels
+
+    # To get the mapping from original labels to encoded labels, you can use:
+    label_mapping = dict(zip(label_encoder.classes_, label_encoder.transform(label_encoder.classes_)))
+
+    logger.info("Saving Dataset...")
+    np.savetxt("data_cache/X_train.csv", X_train, delimiter=",")
+    np.savetxt("data_cache/X_test.csv", X_test, delimiter=",")
+    np.savetxt("data_cache/y_train_encoded.csv", y_train_encoded, delimiter=",")
+    np.savetxt("data_cache/y_test_encoded.csv", y_test_encoded, delimiter=",")
+
+    with open('data_cache/label_mapping.pickle', 'wb') as handle:
+        pickle.dump(label_mapping, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    logger.info("Dataset saved into data_cache folder!")
+
+
+if __name__ == "__main__":
+    start = time.time()
+    logger.info("Starting up..")
+
+    # dataset_path = '/home/thanos/Documents/Thesis/dataset_paths_transformed.txt'
+    dataset_path = '/home/thanos/Documents/Thesis/Dataset_paths/dataset_paths_CQT.txt'
+
+    main(dataset_path)
+
+    time_elapsed = time.time() - start
+    # logger.info(f"Finished, Found {len(converted_test.df)} chords.")
+    logger.info(f"Time elapsed: {time_elapsed:.2f} seconds.")
---
+++ src/deep_cnn/fit_cnn.py
@@ -0,0 +1,94 @@
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import pandas as pd
+import time
+from loguru import logger
+from tensorflow.keras.optimizers import Adam
+import gc
+
+import sys
+import pickle
+
+sys.path.append("../src")
+
+from src.utils.create_dataset import *
+from src.adapt_labels import *
+
+
+def main(dataset_file):
+    """
+
+    :param dataset_file: path of file containing X_train, X_test, y_train_encoded, y_test_encoded and label_mapping pickle
+    :return:
+    """
+
+    X_train = np.loadtxt(f"{dataset_file}/X_train_debug.csv", delimiter=",")
+    X_test = np.loadtxt(f"{dataset_file}/X_test_debug.csv", delimiter=",")
+    y_train_encoded = np.loadtxt(f"{dataset_file}/y_train_encoded_debug.csv", delimiter=",").astype(int)
+    y_test_encoded = np.loadtxt(f"{dataset_file}/y_test_encoded_debug.csv", delimiter=",").astype(int)
+
+    with open(f"{dataset_file}/label_mapping_debug.pickle", 'rb') as lm:
+        label_mapping = pickle.load(lm)
+
+    # Reshape the features for CNN input
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+
+    # Define the CNN model
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv1D(16, kernel_size=3, activation='relu', padding="same", input_shape=(X_train.shape[1], 1)),
+        tf.keras.layers.Conv1D(16, kernel_size=3, padding="same", activation='relu'),
+        tf.keras.layers.MaxPooling1D(pool_size=2),
+        tf.keras.layers.Dropout(0.4),
+        tf.keras.layers.Conv1D(32, kernel_size=3, padding="same", activation='relu'),
+        tf.keras.layers.Conv1D(32, kernel_size=3, padding="same", activation='relu'),
+        tf.keras.layers.MaxPooling1D(pool_size=2),
+        tf.keras.layers.Dropout(0.4),
+        tf.keras.layers.Conv1D(64, kernel_size=3, padding="same", activation='relu'),
+        tf.keras.layers.Conv1D(64, kernel_size=3, padding="same", activation='relu'),
+        tf.keras.layers.MaxPooling1D(pool_size=2),
+        tf.keras.layers.Dropout(0.4),
+        tf.keras.layers.Conv1D(128, kernel_size=3, padding="same", activation='relu'),
+        tf.keras.layers.Conv1D(128, kernel_size=3, padding="same", activation='relu'),
+        tf.keras.layers.MaxPooling1D(pool_size=2),
+        tf.keras.layers.Dropout(0.4),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(len(np.unique(np.concatenate((y_test_encoded, y_train_encoded)))), activation='softmax')
+        # Adjust the number of output units
+    ])
+
+    # Print shapes using print statements
+    for layer in model.layers:
+        print(layer.name, layer.output_shape)
+
+    optimizer = Adam(learning_rate=0.0001)
+
+    # Compile the model
+    model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # Train the model
+    model.fit(X_train, y_train_encoded, epochs=30, batch_size=64, validation_data=(X_test, y_test_encoded))
+
+    # Evaluate the model on the test set
+    test_loss, test_accuracy = model.evaluate(X_test, y_test_encoded)
+    print(f'Test Accuracy: {test_accuracy * 100:.2f}%')
+
+    # Save the model if needed
+    model.save('models/CQT_cnn_root_debug.h5')
+
+
+if __name__ == "__main__":
+    start = time.time()
+    logger.info("Starting up..")
+
+    # dataset_path = '/home/thanos/Documents/Thesis/dataset_paths_transformed.txt'
+    dataset_file = 'data_cache'
+
+    main(dataset_file)
+
+    time_elapsed = time.time() - start
+    # logger.info(f"Finished, Found {len(converted_test.df)} chords.")
+    logger.info(f"Time elapsed: {time_elapsed:.2f} seconds.")
