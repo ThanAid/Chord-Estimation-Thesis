eb5512dc735d8597ccf33407e0e5bee3a34c361f
ThanAid <thanos.aidinis@gmail.com>
2024-05-19 17:52:41 +0300

    updating resutls

--- src/utils/plot_models.py
+++ src/utils/plot_models.py
@@ -1,8 +1,8 @@
 from tensorflow.keras.utils import plot_model
 import tensorflow as tf
 
-model = tf.keras.models.load_model("/home/thanos/Documents/Thesis/Chord-Estimation-Thesis/src/naive_cnn/naive_cnn.h5")
+model = tf.keras.models.load_model("/home/thanos/Documents/Thesis/Chord-Estimation-Thesis/src/deep_cnn/models/CQT_cnn_root_10.h5")
 
 model.summary()
 
-plot_model(model, to_file="/home/thanos/Documents/Thesis/model_plots/naive_cnn.png", show_shapes=True)
+plot_model(model, to_file="/home/thanos/Documents/Thesis/model_plots/model_2_root.png", show_shapes=True)
